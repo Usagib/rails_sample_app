680e64146dc6e049071d724d14c1246e3139b961
conejitomaya <eduardobonillac@gmail.com>
2020-02-06 01:57:39 -0600

    contact page added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
   resources :users
-  root 'application#hello'
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
