1221eb5bd872c3bc3e91790460be01018f9f36c0
conejitomaya <eduardobonillac@gmail.com>
2020-02-05 23:51:31 -0600

    StaticPages controlller with home help,added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   resources :users
   root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
