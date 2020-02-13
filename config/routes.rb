e3404a5bafecc469cdba26563d75d066f4deb7ed
conejitomaya <eduardobonillac@gmail.com>
2020-02-13 09:45:40 -0600

    signup page added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
   resources :users
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,7 +11,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-    get new_user_url
+    get signup_path
     assert_response :success
   end
 
