bf1e80ad5168e2e3380f04224aa8e7985b8db0cd
conejitomaya <eduardobonillac@gmail.com>
2020-02-24 13:44:18 -0600

    app reset

--- app/views/users/_user.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! user, :id, :name, :email, :code, :created_at, :updated_at
-json.url user_url(user, format: :json)
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-  root 'static_pages#home'
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
